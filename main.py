3ab3573ae1f16617ca724aef0309efdee1daa4cc
Dan Haski <dhaski@gmail.com>
2023-03-06 16:34:54 +0200

    Done

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ except FileNotFoundError:
     FRENCH_WORDS_DATA = pandas.read_csv("data/french_words.csv")
     df = pandas.DataFrame(FRENCH_WORDS_DATA)
     words_to_learn = df.values.tolist()
-    print(words_to_learn)
 
 
 # ---------------------------- SAVE WORDS TO LEARN ------------------------------- #
@@ -25,7 +24,7 @@ def save_progress():
     data = pandas.DataFrame(words_to_learn)
     data.to_csv("data/words_to_learn.csv", index=False)
 
-# ---------------------------- DID YOU KNOW THE WORD ------------------------------- #
+# ---------------------------- KNEW THE WORD ------------------------------- #
 
 
 def knew_the_word():
@@ -45,10 +44,6 @@ def knew_the_word():
             print("The file does not exist")
 
 
-def didnt_know_the_word():
-    show_next_card()
-
-
 # ---------------------------- SHOW NEXT WORD ------------------------------- #
 current_card = {}
 
@@ -57,7 +52,6 @@ def show_next_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
     current_card = random.choice(words_to_learn)
-    print(current_card)
     canvas.itemconfig(canvas_image, image=CARD_FRONT)
     canvas.itemconfig(title_text, text="Français", fill="black")
     canvas.itemconfig(word_text, text=current_card[0], fill="black")
@@ -66,7 +60,6 @@ def show_next_card():
 
 def flip_card():
     global current_card
-    print(current_card)
     canvas.itemconfig(canvas_image, image=CARD_BACK)
     canvas.itemconfig(title_text, text="English", fill="white")
     canvas.itemconfig(word_text, text=current_card[1], fill="white")
@@ -95,7 +88,7 @@ word_text = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 
 # Buttons
 wrong_button = Button(image=WRONG_BUTTON_IMG, highlightthickness=0, borderwidth=0, width=97, height=97,
-                      command=didnt_know_the_word)
+                      command=show_next_card)
 wrong_button.grid(row=1, column=0)
 
 right_button = Button(image=RIGHT_BUTTON_IMG, highlightthickness=0, borderwidth=0, width=97, height=97,
